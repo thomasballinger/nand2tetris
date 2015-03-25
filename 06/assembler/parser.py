ac04c0a385f1ff8a3cc1f72d65cd3fb3ac268e31
Thomas Ballinger <thomasballinger@gmail.com>
2015-03-25 13:45:50 -0400

    trying making parse a function

--- 06/assembler/parser.py
+++ 06/assembler/parser.py
@@ -27,34 +27,35 @@ def without_blanks(lines):
             yield line
 
 
+def parse(assembly_file, symbol_table):
+    with open(assembly_file, 'r') as raw_code:
+        code = []
+        addr = 0
+
+        for line in without_blanks(comments_stripped(stripped(raw_code))):
+
+            # Check for and store user-defined symbols
+            if is_label(line):
+                label = label_from_line(line)
+                if is_symbol(label):
+                    symbol_table.add_entry(label, addr)
+
+            code.append(line)
+
+            # Only increment address if line is not a label; labels
+            # don't produce instructions
+            if line[0] != '(':
+                addr += 1
+
+    return code
+
+
 class Parser:
     """Parse an assembly program"""
     def __init__(self, assembly_file):
         """Open filestream for assembly file and prepare to parse it."""
         self.symbol_table = SymbolTable()
-        self.code = self.parse(assembly_file)
-
-    def parse(self, assembly_file):
-        with open(assembly_file, 'r') as raw_code:
-            code = []
-            addr = 0
-
-            for line in without_blanks(comments_stripped(stripped(raw_code))):
-
-                # Check for and store user-defined symbols
-                if is_label(line):
-                    label = label_from_line(line)
-                    if is_symbol(label):
-                        self.symbol_table.add_entry(label, addr)
-
-                code.append(line)
-
-                # Only increment address if line is not a label; labels
-                # don't produce instructions
-                if line[0] != '(':
-                    addr += 1
-
-        return code
+        self.code = parse(assembly_file, self.symbol_table)
 
     def set_current_command(self, command_index):
         """Set which command is being analysed currently."""

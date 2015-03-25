7326c98905b92cba3b5cf2e644fdd22c11b59cb0
Thomas Ballinger <thomasballinger@gmail.com>
2015-03-25 13:21:35 -0400

    more explicit check_table

--- 06/assembler/check_table.py
+++ 06/assembler/check_table.py
@@ -1,15 +1,18 @@
-def check_table(table, symbol, line_number):
+def check_table(table, line, line_number):
     """Preliminary check through assembly to find user-defined symbols.
 
     Any symbols found can then be added to SymbolTable for future use.
     """
 
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if is_label(line):
+        symbol = line.strip('()')
 
-    if symbol[0] == '(':
-        symbol = symbol.strip('()')
-    else:
-        return
+        if not table.contains(symbol) and not symbol.isdigit():
+            table.add_entry(symbol, line_number)
 
-    if not table.contains(symbol) and symbol[0] not in numbers:
-        table.add_entry(symbol, line_number)
+
+def is_label(line):
+    """Returns whether a line is a label
+
+    line must be already whitespace-stripped"""
+    return line.startswith('(')
